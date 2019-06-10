8e7f38cb83f4a4a4cf9333aedcef4ed25d4eb334
Jeromy Cannon <jeromylewiscannon@gmail.com>
2019-06-10 17:36:55 -0500

    added express

--- node/index.js
+++ node/index.js
@@ -1,6 +1,15 @@
-require('dotenv').config()
+require('dotenv').config({ debug: process.env.DEBUG }); // https://www.npmjs.com/package/dotenv
 const bunyan = require('bunyan');
 const log = bunyan.createLogger({name: "logger"});
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 log.info("current log level: ", log.level());
 // $ node index.js | .\node_modules\.bin\bunyan
 
+app.listen(3000, 'localhost', function () {
+    log.info(new Date() + ': Server has started! http://localhost:3000');
+});
