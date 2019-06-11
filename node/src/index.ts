029d5206629e10181b95af4c787b89a27d14dfa3
Jeromy Cannon <jeromylewiscannon@gmail.com>
2019-06-10 21:13:56 -0500

    finished sample rest server

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,34 +1,46 @@
 // https://www.npmjs.com/package/dotenv
 const dotenv = require('dotenv');
 const bunyan = require('bunyan');
-
-
-dotenv.config({
-    debug: process.env.DEBUG
-});
-
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const log = bunyan.createLogger({
     name: "logger"
 });
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-log.info("current log level: ", log.level());
-// $ node index.js | .\node_modules\.bin\bunyan
 
-app.get('/api/v1/todos', (req, res) => {
+function init() {
+    dotenv.config({
+        debug: process.env.DEBUG
+    });
+
+    app.set('view engine', 'ejs');
+    app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    log.info("current log level: ", log.level());
+    // $ node index.js | .\node_modules\.bin\bunyan
+
+}
+
+init();
+
+app.get('/rest/mc/backup', (req, res) => {
     res.status(200).send({
         success: 'true',
-        message: 'todos retrieved successfully'
+        message: 'minecraft backup complete'
+    })
+});
+
+app.get('/rest/mc/restore', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'minecraft restored to last backup'
     })
 });
 
 app.listen(3000, 'localhost', function () {
-    log.info(process.env.TEST);
+    log.info(process.env.GREETING);
     log.info(new Date() + ': Server has started! http://localhost:3000');
 });
