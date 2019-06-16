04fa7bad9f0ba6498969ba61c1a1f7647627f751
Jeromy Cannon <jeromylewiscannon@gmail.com>
2019-06-16 10:05:05 -0500

    updated to make calls based on .env parameters

--- node/src/index.ts
+++ node/src/index.ts
@@ -5,6 +5,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
 const log = bunyan.createLogger({
     name: "logger"
 });
@@ -26,11 +29,24 @@ function init() {
 
 }
 
+async function restore() {
+    const { stdout, stderr } = await exec(process.env.RESTORE_CMD);
+    console.log('restore stdout:', stdout);
+    console.log('restore stderr:', stderr);
+}
+
+async function backup() {
+    const { stdout, stderr } = await exec(process.env.BACKUP_CMD);
+    console.log('backup stdout:', stdout);
+    console.log('backup stderr:', stderr);
+}
+
 init();
 
 const backupUrl = '/rest/mc/backup';
 app.get(backupUrl, (req, res) => {
     log.info('backing up...');
+    backup();
     res.status(200).send({
         success: 'true',
         message: 'minecraft backup complete'
@@ -41,6 +57,7 @@ app.get(backupUrl, (req, res) => {
 const restoreUrl = '/rest/mc/restore';
 app.get(restoreUrl, (req, res) => {
     log.info('restoring...');
+    restore();
     res.status(200).send({
         success: 'true',
         message: 'minecraft restored to last backup'
